71dc247bb72391a0f1f00b13aeab7582cea40e2d
THINESHKUMAR <thinesh12@gmal.com>
2023-04-24 06:40:56 -0400

    edit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+       method: "POST",
+       headers: {
+           'content-type': 'application/json',
+           'accept': 'application/json'
+       },
+       body: JSON.stringify({
+           name: `${name}`,
+           email: `${email}`
+       }),
+   })
+   .then(response => response.json())
+   .then(function(data) {
+       const newDiv = document.createElement('div')
+       document.querySelector('body').appendChild(newDiv)
+       document.querySelector('div').innerHTML = data.id
+   })
+   .catch(function (error) {
+       alert("Unauthorized Access");
+       const newDiv2 = document.createElement("div")
+       newDiv2.id = "Error"
+       document.querySelector("body").appendChild(newDiv2)
+       document.getElementById("Error").innerHTML = error.message
+})
+}
